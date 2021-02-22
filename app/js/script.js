33b8be26e6a59232ec9828b996d134c21ad05c79
Laura THAY <laura.thay@ipag.fr>
2021-02-22 15:33:17 +0100

    fade in fade out animation
    on essaie de faire en sorte que lorsqu'on clic sur le bouton hamburger le fade in apparaisse et quand on reclic fade out

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,22 @@
 
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = documement.querySelector('.overlay');
 
 //pour que le burger devienne une croix 
 
-btnHamburger.addEventListener('click', function(){
+btnHamburger.addEventListener('click', function(){ //close hamburger menu
     console.log('click hamburger');
 
     if(header.classList.contains('open')){
         header.classList.remove('open');
-    }else{
-        btnHamburger.classList.add('open')
+        header.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else{ // open hamburger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 
 });

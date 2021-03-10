726b771cd093ea02dd0d40ccc50699dc03db91f4
Laura THAY <laura.thay@ipag.fr>
2021-03-10 13:57:50 +0100

    added

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadedElems = document.querySelectorAll('.has-faded');
@@ -10,6 +11,7 @@ btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){ //close hamburger menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         fadedElems.forEach(function(element){
             element.classList.remove('fade-in');
@@ -17,6 +19,7 @@ btnHamburger.addEventListener('click', function(){
         });
     }
     else{ // open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
         fadedElems.forEach(function(element){
             element.classList.remove('fade-out');
